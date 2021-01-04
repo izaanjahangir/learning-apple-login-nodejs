e8c5698137cf5db7e7832e03389e81d3c5036565
izaanjahangir <izaanjahangir2@gmail.com>
2021-01-04 13:02:09 +0500

    added logic

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 const express = require("express");
 const app = express();
 
+const appleSignin = require("apple-signin");
+
 app.use(express.json());
 
-app.post("/apple-login", (req, res) => {
+app.post("/apple-login", async (req, res) => {
   try {
+    const APPLE_CLIENT_ID = "PUT_YOUR_APPLE_CLIENT_ID";
+
+    // Client side will authenticate user from client app and send you idToken and email
+    const { idToken, email } = req.body;
+
+    const result = await appleSignin.verifyIdToken(idToken, APPLE_CLIENT_ID);
+
+    // This will contain the unique identifer for this user from apple
+    const appleId = result.sub;
+
+    // Now we check if appleId already exist in our database
+    // If it exist then we skip the user creation process and generate a JWT and send it to client
+    // else we will first create user and set it's appleId and email then generate a JWT and send it to client
+
     res.json({
       success: true,
       message: "User logged in successfully",
