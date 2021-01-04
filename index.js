7d1ae66572fb13ebfbb49209742322540470a402
izaanjahangir <izaanjahangir2@gmail.com>
2021-01-04 12:34:53 +0500

    added starter code

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
+app.post("/apple-login", (req, res) => {
+  try {
+    res.json({
+      success: true,
+      message: "User logged in successfully",
+      data: {},
+    });
+  } catch (e) {
+    res.status(401).json({ success: false, message: e.message });
+  }
+});
+
 app.listen(5000, () => {
   console.log("Server is running on port 5000");
 });
